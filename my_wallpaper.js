49907b1b997f350cc8a1d89b3e23592ba03022c0
Aymsi <agknott23@gmail.com>
2025-07-28 16:44:29 +1200

    Testing with my original flower design
    First test with original flower design. I'm not sure how to make this repeat across a larger area yet

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -17,9 +17,37 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(0); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+ 
+  var petallength = 80;
+
+  noStroke()
+  fill(255, 66, 167)
+  //outside back petals
+  ellipse(135, 65, petallength, petallength);
+  ellipse(65, 65, petallength, petallength);
+  ellipse(65, 135, petallength, petallength);
+  ellipse(135, 135, petallength, petallength);
+
+  fill(247, 119, 187)
+  //petals
+  ellipse(100, 145, 45, petallength);
+  ellipse(100, 55, 45, petallength);
+  ellipse(150, 100, petallength, 45);
+  ellipse(50, 100, petallength, 45);
+
+  fill(255, 166, 213)
+  //insidepetals
+  ellipse(125, 75, petallength - 30, 50);
+  ellipse(75, 75, petallength - 30, 50);
+  ellipse(75, 125, petallength - 30, 50);
+  ellipse(125, 125, petallength - 30, 50);
+
+  fill(255, 247, 138)
+  //centre circle
+  ellipse(100, 100, 45, 45);
+  
 }
