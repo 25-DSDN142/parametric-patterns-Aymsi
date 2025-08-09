2219e4a02f985f6d6ccffaf607286f5e9233f065
Aymsi <agknott23@gmail.com>
2025-08-10 00:26:14 +1200

    completely reconstructing the code under just the drawstar function
    hopefully this way I could draw multiple stars? under the mysymbol function?

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -20,7 +20,7 @@ let squarewidth = 70
 let trianglelength = 50
 
 //custom stroke weight variables + used for if statements
-let standardstroke = 5
+let standardstroke = 2
 
 //colour variables 
 //each colour variable had to be put under each layer triangle function to work properly
@@ -45,7 +45,7 @@ let standardstroke = 5
 let scaleFactor = 1
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   //pWallpaper.output_mode(GRID_WALLPAPER) - change back to DEVELOP_GLYPH to edit just design
   
   pWallpaper.resolution(FIT_TO_SCREEN); //FIT_TO_SCREEN //NINE_PORTRAIT makes it portrait repeated 9x //can also do NINE_LANDSCAPE
@@ -73,30 +73,20 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 //    scaleFactor = 1
 // }
   DrawStar();
-  DrawSecondStarLayer();
-  DrawThirdStarLayer();
-  DrawFourthStarLayer();
-  DrawFifthStarLayer();
-  DrawSixthStarLayer();
-  DrawFrontStarLayer();
 
 }
 
-
-//Seperated each layer of the star into its own function - for clarity of code and easier adjustment 
-//making each function based around the centrex = 100, and centrey = 100 function
-function DrawStar () { //DrawBackStarLayer
+function DrawStar () { 
 //scale(scaleFactor);
 
 //colour functions (need to be here so it runs properly)
   let basestrokec = color(199, 236, 255);
   let thickstrokec = color(255);
   let brightyellow = color(255, 218, 97);
+  let lightyellow = color(255, 244, 191);
+  let lightblue = color(181, 227, 255);
 
-//Drawing back star layer
-//drawing shape with vertex points to form shape as if i was making the triangles based off of a square's points
-  fill(brightyellow); //bright yellow original
-
+//if function for stroke to turn white if its thicker 
   if ((standardstroke + 2) > 2) {
     stroke(thickstrokec); // white stroke for thicker standard stroke + 2
   } else {
@@ -104,10 +94,12 @@ function DrawStar () { //DrawBackStarLayer
   }
 
   strokeWeight(standardstroke + 2); //thicker back stroke w custom variable
-  //15, 45, 66 (option) //145, 193, 230 (option) //changing stroke colour to dark blue tones
+  //colour options:15, 45, 66 (option) //145, 193, 230 (option) //changing stroke colour to dark blue tones
+  fill(brightyellow); //bright yellow original
   
+  //Drawing back star layer
   beginShape();
-
+  //creating each point of triangle shapes
   vertex(leftpoint, centrey - 20);
   vertex(centrex, outsidestarpointlength - connecttoedge);
   vertex(rightpoint, centrey - 20);
@@ -119,30 +111,20 @@ function DrawStar () { //DrawBackStarLayer
   
   endShape(CLOSE);
 
-}
+//Drawing second star layer - Behind square shape - was going to do triangles but square more efficient!
 
-function DrawSecondStarLayer () {
-  scale(scaleFactor);
-
-  let brightyellow = color(255, 218, 97);
- //Behind square shape - was going to do triangles but square more efficient!
   fill(brightyellow); //bright yellow original
   strokeWeight(standardstroke);
-  stroke(199, 236, 255);
+  stroke(basestrokec);
   square(centrex - 35, centrey - 35, squarewidth);
 
-}
+//Drawing third star layer - centre diamond of back star
 
-function DrawThirdStarLayer () {
-  scale(scaleFactor);
-
-  let lightyellow = color(255, 244, 191);
-  //centre diamond of back star
   fill(lightyellow); //lightest yellow original
   strokeWeight (standardstroke);
   stroke(199, 236, 255);
-  beginShape();
 
+  beginShape();
   //adding each point of the diamond
   vertex(centrex, centrey - 40);
   vertex(centrex + 40, centrey);
@@ -151,27 +133,18 @@ function DrawThirdStarLayer () {
 
   endShape(CLOSE);
 
-}
+//Drawing fourth star layer - diagonal middle triangles
 
-function DrawFourthStarLayer () {
-  scale(scaleFactor);
-
-  let basestrokec = color(199, 236, 255);
-  let thickstrokec = color(255);
-
-let lightblue = color(181, 227, 255);
-fill(lightblue); //light blue original
-
+//if function for stroke to turn white if its thicker 
   if ((standardstroke + 2) > 2) {
     stroke(thickstrokec); // white stroke for thicker standard stroke + 2
   } else {
     stroke(basestrokec); // normal blue colour stroke for normal standardstroke
   }
-
+  
+  fill(lightblue); //light blue original
   strokeWeight (standardstroke + 2);
 
-  //diagonal middle triangles
-  //top right triangle
   //first.x, first.y, second.x, second.y, third.x, third.y
   triangle(centrex - 10, centrey - 10, centrex + 10, centrey + 10, centrex + trianglelength, centrey - trianglelength);
   //bottom right triangle
@@ -181,61 +154,42 @@ fill(lightblue); //light blue original
   //toplefttriangle
   triangle(centrex - 10, centrey + 10, centrex + 10, centrey - 10, centrex - trianglelength, centrey - trianglelength);
 
-}
+//Drawing fifth star layer - centre square of star
 
-function DrawFifthStarLayer () {
-  scale(scaleFactor);
-
-let brightyellow = color(255, 218, 97);
-  //centre square of star
   fill(brightyellow); //bright yellow original
   strokeWeight (standardstroke);
-  stroke(199, 236, 255);
+  stroke(basestrokec);
   square(centrex - 10, centrey - 10, squarewidth - 50);
 
-}
+//Drawing sixth star layer - inside star triangles
 
-function DrawSixthStarLayer () {
-  scale(scaleFactor);
-//inside star triangles
- //topinsidetriangle
-let basestrokec = color(199, 236, 255);
-  let thickstrokec = color(255);
-
-let lightyellow = color(255, 244, 191);
-
-  fill(lightyellow); //light yellow original
-
-    if ((standardstroke + 1) > 2) {
+//if function for stroke to turn white if its thicker 
+  if ((standardstroke + 2) > 2) {
     stroke(thickstrokec); // white stroke for thicker standard stroke + 2
   } else {
     stroke(basestrokec); // normal blue colour stroke for normal standardstroke
   }
 
+  fill(lightyellow); //light yellow original
   strokeWeight (standardstroke + 1);
   
   //first.x, first.y, second.x, second.y, third.x, third.y
+  //top inside triangle
   triangle(centrex - 10, centrey - 10, centrex + 10, centrey - 10, centrex, centrey - trianglelength);
-  //rightinsidetriangle
+  //right inside triangle
   triangle(centrex + 10, centrey - 10, centrex + 10, centrey + 10, centrex + trianglelength, centrey);
-  //bottominsidetriangle
+  //bottom inside triangle
   triangle(centrex - 10, centrey + 10, centrex + 10, centrey + 10, centrex, centrey + trianglelength);
-  //leftinsidetriangle
+  //left inside triangle
   triangle(centrex - 10, centrey - 10, centrex - 10, centrey + 10, centrex - trianglelength, centrey);
 
-}
-
-function DrawFrontStarLayer () {
-  scale(scaleFactor);
-
-  //very centre star diamond
-  //(ontop of centre square)
-  let lightyellow = color(255, 244, 191);
+//Drawing front star layer - very centre front star diamond ontop of centre square
 
   fill(lightyellow); //light yellow original
   strokeWeight(standardstroke - 0.5);
-  stroke(199, 236, 255);
-   beginShape();
+  stroke(basestrokec);
+
+  beginShape();
 
   //adding each point of the diamond
   vertex(centrex, centrey - 10);
@@ -246,7 +200,3 @@ function DrawFrontStarLayer () {
   endShape(CLOSE);
 
 }
-
- //the goal is for every triangle of the piece to be a different shade of the star, looking like its shining
-  //gives perameters to change, i wonder if i could randomly change from a selection of colours?
-  //how to add an if statement?
